d6e12930c622fe1faf2c2ff7f501bc9ee93b58a4
Sachindra Singh <sachindras@spc.int>
2023-10-25 10:04:36 +1100

    Video Updated.

--- src/pages/Library.js
+++ src/pages/Library.js
@@ -12,7 +12,7 @@ const Library = () => {
     <DefaultBanner>
       <h1>Documents Library</h1>
       <p>
-      Repository of documents, media and other assets relevant to Digital Earth Pacific.
+        Repository of documents, media and other assets relevant to Digital Earth Pacific.
       </p>
     </DefaultBanner>
   );
@@ -25,14 +25,17 @@ const Library = () => {
     <Layout bannerHeader={banner} isShort>
       <SEO title="Library" />
 
-      <div align="center" style={{background: "black", marginTop: -20}}>    
-        <br/>    
-        <iframe width="700" height="394" src="https://www.youtube.com/embed/jCWyfXRIFe0" title="Digital Earth Pacific" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        <br/>
+      <div align="center" style={{ background: "black", marginTop: -20 }}>
+        <br />
+        {/* <iframe width="700" height="394" src="https://www.youtube.com/embed/jCWyfXRIFe0" title="Digital Earth Pacific" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+
+        <iframe src="https://spccloud.sharepoint.com/sites/commsteam/_layouts/15/embed.aspx?UniqueId=f91734ab-ff55-45bf-a755-c33c6ecf7c1d&embed=%7B%22hvm%22%3Atrue%2C%22ust%22%3Atrue%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create" width="740" height="460" frameborder="0" scrolling="no" allowfullscreen title="DEP_Final (25.10) FLINCH.mov"></iframe>
+
+        <br />
       </div>
-      
-      <br/>
-      <br/>
+
+      <br />
+      <br />
 
       <div className="layout-container">
         <div className="layout-row grid-content">{appsList}</div>
