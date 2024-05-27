49b4359bad0affe5758a67a8d84e725dfcde2d6a
Sachindra Singh <sachindras@spc.int>
2024-05-27 10:32:56 -0400

    Update UNSIDS.js

--- src/pages/UNSIDS.js
+++ src/pages/UNSIDS.js
@@ -17,7 +17,7 @@ const UNSIDS = () => {
       </p>
       <br />
       <p>
-        <b>Date and Venue:</b><br />Thursday, 30 May 2024, Room 5
+        <b>Date and Venue:</b><br />Thursday, 30 May 2024, Room 5, 08:00 - 09:30
       </p>
       <br />
       <p>
